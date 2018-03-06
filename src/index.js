3f86256a08789e4e776e1cf380796c7aa2b9d219
Maurice Ngo <igotmauxy@gmail.com>
2018-03-06 02:31:48 -0800

    converted to webpack plugin

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 #!/usr/bin/env node --harmony
-const defaults = {
-  excludes: ['_windup.scss']
-};
 
 // Depends
+const _ = require('lodash');
 const FS = require('fs');
 const PATH = require('path');
 const getFiles = require('./get-files');
 const ROOT_PATH = './scss';
+const camelCase = require('camelcase');
 const jsonfile = require('jsonfile');
+
+const defaults = {
+  scssDir: './scss',
+  jsonDir: './',
+  excludes: ['_windup.scss']
+};
 
 class Tenacity {
   constructor(options) {
     this.options = Object.assign({}, defaults, options);
-    this.init();
+    //this.init();
   }
   removeWhiteSpace(str) {
     return str.replace(/ /g, '');
@@ -33,7 +38,10 @@ class Tenacity {
     const contentArray = globalContent.split(';');
     contentArray.forEach(styleProperties => {
       const styleProperty = styleProperties.replace(':', '\x01').split('\x01');
-      data[styleProperty[0]] = styleProperty[1];
+      if (styleProperty[1]) {
+        const styleName = camelCase(styleProperty[0].replace('$', ''));
+        data[styleName] = styleProperty[1];
+      }
     });
     return data;
   }
@@ -45,21 +53,41 @@ class Tenacity {
     return this.objectifySass(cleanContent);
   }
   writeJsonFile(data) {
-    var file = './styleguide.json';
-    jsonfile.writeFile(file, data, { spaces: 2 }, function(err) {
-      console.error(err);
+    const file = PATH.join(this.options.jsonDir, 'styleguide.json');
+    jsonfile.writeFile(file, data, { spaces: 2 }, err => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('\x1b[1m%s\x1b[0m', 'TENACITY');
+        console.log(
+          '\x1b[32m%s\x1b[0m',
+          `${PATH.basename(this.options.scssDir)} converted to ${PATH.basename(
+            file
+          )}`
+        );
+      }
     });
   }
   init() {
-    const dirData = getFiles(ROOT_PATH, this.options);
-    console.log(JSON.stringify(dirData, null, 4));
+    const dirData = getFiles(this.options.scssDir, this.options);
     const globalData = this.getSassData(dirData, '_global.scss');
     const overridesData = this.getSassData(dirData, '_overrides.scss');
-    const mergedData = Object.assign({}, globalData, overridesData);
-    this.writeJsonFile(mergedData);
+    const mergedData = _.merge(globalData, overridesData);
+    const file = PATH.join(this.options.jsonDir, 'styleguide.json');
+    jsonfile.readFile(file, (err, syleguideJsonData) => {
+      if (!_.isEqual(syleguideJsonData, mergedData)) {
+        this.writeJsonFile(mergedData);
+      }
+    });
+  }
+  apply(compiler) {
+    compiler.plugin('done', () => {
+      this.init();
+    });
   }
 }
 
-new Tenacity();
-// module.exports = Tenacity;
-// module.exports.Options = Tenacity;
+//new Tenacity();
+
+module.exports = Tenacity;
+module.exports.Options = Tenacity;
